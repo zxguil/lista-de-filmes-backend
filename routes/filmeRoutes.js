e49539f697adb6a943e9cb4ba0a99056a7c02229
zxguil <zxguil@gmail.com>
2022-07-12 21:11:40 -0400

    style: alteracao na formatação do código

--- routes/filmeRoutes.js
+++ routes/filmeRoutes.js
@@ -1,103 +1,99 @@
 const router = require('express').Router();
-var Filme = require('../app/models/filme');
-
-async function getFilme(id) {
-    const filme = await Filme.findOne({ _id: id });
-    if (!filme) {
-        res.status(422).json({ message: 'Filme não encontrado!' })
-    }
-    return filme;
-}
+const Filme = require('../app/models/filme');
 
 router.post('/', async (req, res) => {
     const { nome } = req.body;
-    const filme = { nome }
-    
-    if ( nome == "" ) {
-        res.status(400).json({ error: "O nome não pode estar vazio." })
+    const filme = { nome };
+
+    if (nome === '') {
+        res.status(400).json({ error: 'Informe o nome do filme.' });
         return;
     }
-    
+
     try {
         const newFilme = await Filme.create(filme);
-        res.status(201).json({ "filme": newFilme, message: "Filme cadastrado com sucesso!"});
+        res.status(201).json({
+            filme: newFilme,
+            message: 'Filme cadastrado com sucesso!',
+        });
     } catch (error) {
-        res.status(500).json({ "error": error });
+        res.status(500).json({ error: 'Erro ao cadastrar filme.' });
     }
 });
-router.get('/', async (req, res) => {   
+router.get('/', async (req, res) => {
     try {
         const filmes = await Filme.find();
         res.status(200).json(filmes);
     } catch (error) {
-        res.status(500).json({ error: "Erro ao selecionar todos: " + error });
+        res.status(500).json({ error: 'Erro ao selecionar todos.' });
     }
 });
 router.patch('/:id', async (req, res) => {
-    const id = req.params.id;
-    const {nome, assistido} = req.body;
+    const { id } = req.params;
+    const { nome, assistido } = req.body;
     const filme = {
         nome,
-        assistido
-    }
+        assistido,
+    };
 
     if (!id) {
-        res.status(500).json({ error: "É necessário enviar id" })
+        res.status(500).json({ error: 'É necessário enviar id.' });
         return;
     }
-    
-    if ( nome == "" ) {
-        res.status(400).json({ error: "O nome não pode estar vazio." })
+
+    if (nome === '') {
+        res.status(400).json({ message: 'Informe o nome do filme.' });
         return;
     }
 
     try {
-        let updateFilme = null;
-        if (assistido  === undefined && !nome) {
-            res.status(500).json({ error: "É necessário enviar ao menos um parametro" })
+        let updatedFilme;
+        if (assistido === undefined && !nome) {
+            res.status(500).json({
+                error: 'É necessário enviar ao menos um parametro.',
+            });
             return;
         }
-        
+
         if (assistido === undefined && nome) {
-            updatedFilme = await Filme.updateOne({_id: id}, 
-                {$set: { "nome": nome } }
-            );
-        } 
-        
-        if (!nome) {
-            updatedFilme = await Filme.updateOne({_id: id}, 
-                { $set: { "assistido": assistido } }
+            updatedFilme = await Filme.updateOne(
+                { _id: id },
+                { $set: { nome } },
             );
         }
-        
-        if(assistido !== undefined){
-            updatedFilme = await Filme.updateOne({_id: id}, filme);
+
+        if (!nome) {
+            updatedFilme = await Filme.updateOne(
+                { _id: id },
+                { $set: { assistido } },
+            );
+        }
+        if (assistido !== undefined) {
+            updatedFilme = await Filme.updateOne({ _id: id }, filme);
         }
 
         if (updatedFilme.matchedCount === 0) {
-            res.status(422).json({ message: "O filme não foi encontrado!" });
+            res.status(422).json({ error: 'O filme não foi encontrado!' });
             return;
         }
 
-        res.status(200).json({message: "Atualizado com sucesso"})
+        res.status(200).json({ message: 'Atualizado com sucesso' });
     } catch (error) {
-        res.status(500).json({ error: "Erro ao atualizar dados: "+error })
+        res.status(500).json({ error: 'Erro ao atualizar dados' });
     }
 });
 router.delete('/:id', async (req, res) => {
-    
     try {
-        const filme = await Filme.deleteOne({_id: req.params.id});
+        const filme = await Filme.deleteOne({ _id: req.params.id });
 
         if (filme.deletedCount === 0) {
             res.status(422).json({ error: 'Filme não encontrado!' });
             return;
         }
-        
-        res.status(200).json({ message: 'Filme removido com sucesso' });
+        res.status(200).json({ message: 'Filme removido com sucesso!' });
     } catch (error) {
-        res.status(500).json({ error: "Filme não encontrado: " + error });
-    }     
+        res.status(500).json({ error: 'Filme não encontrado' });
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,55 +1,40 @@
-//Chamadas dos pacotes:
-require('dotenv').config();  
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
 
-//Definindo a porta onde será executada a nossa api:
-var port = process.env.port || 8000;
+const port = process.env.port || 8000;
 
-
-//Configurando variáveis para acesso ao mongodb
 const username = process.env.DB_USER;
 const password = process.env.DB_PASS;
 const uri = `mongodb+srv://${username}:${password}@clusterfilmes.kl6ju.mongodb.net/?retryWrites=true&w=majority`;
 
-//Conexão mongoose
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-}).then(() => {
-    console.log('Conectado ao mongo');
-    //Iniciando a API junto a conexão com o mongo
-    app.listen(port, () => {
-        console.log("Iniciando a app na porta " + port);
-    });
-}).catch((e) => {
-    console.log('Erro ao conectar: ' +e);
-});
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+    })
+    .then(() => {
+        app.listen(port);
+    })
+    .catch({});
 
-//Configurando o express para ler JSON
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-//Rotas da nossa API:
-//=============================================================================
-var router = express.Router();
+const router = express.Router();
 const filmeRoutes = require('./routes/filmeRoutes');
 
-//Middleware para usar em todos os requests enviados para a nossa API- Mensagem Padrão:
-router.use(function(req, res, next) {
-    console.log('Algo está acontecendo aqui...');
+router.use((req, res, next) => {
     next();
 });
 
-//Rota de Teste para sabermos se tudo está realmente funcionando (acessar através: GET: http://localhost:8000/api): 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
     res.json({ message: 'Olá! Bem vindo(a) ao nosso App' });
 });
 
-//Definindo um padrão das rotas prefixadas: '/api':
 app.use('/api', router);
 
-//Definindo a rota de filmes: '/api/filmes'
-router.use('/filmes', filmeRoutes);  
+router.use('/filmes', filmeRoutes);
